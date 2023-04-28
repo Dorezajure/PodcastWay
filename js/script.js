81f52e06b20d1f882ec2a63d706a6ba5f42ccd44
Daniel_Kovalev <103295803+Dorezajure@users.noreply.github.com>
2023-04-28 12:41:41 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.querySelector(".course__card__btn").addEventListener('click', function() {
+    const course = course__card.classList.add(".hidden");
+    console.log(course);
+});
